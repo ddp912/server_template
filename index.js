9810fa7e1c098f5b5eac9f02aed3146a4a21723a
Donn Malabanan <donnmalabanan@Donns-MacBook-Air.local>
2020-03-17 00:15:48 +0800

    added router

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//routers
+const mainRouter = require('./router/main');
+
+const app = express();
+
+//Main Router
+app.use(mainRouter);
+
+//404 - Page Not Found
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+
+
+app.listen(3000);
