593eee9a92557c82f44ec7abf3aeefdb73c39ad8
Donn Malabanan <donnmalabanan@Donns-MacBook-Air.local>
2020-03-19 08:24:06 +0800

    added mongoDB and static serving

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
 
-//routers
+//dotenv
+require('dotenv').config();
+
+//router
 const mainRouter = require('./router/main');
 
+//Initialize express app
 const app = express();
 
+//EJS View Engine
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+//Serve static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Request body Parser
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Main Router
 app.use(mainRouter);
@@ -17,5 +30,18 @@ app.use((req, res, next) => {
     res.sendStatus(404);
 });
 
+//MongoDB Connection
+const dbUser = process.env.DBUSER;
+const dbPass = process.env.DBPASSWORD;
 
-app.listen(3000);
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0-zxtkz.mongodb.net/test?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('DB Connection Successful! Listening in Port 3000');
+        app.listen(3000);
+    }
+});
